21a655bc5a8587baf13ffb07d4b9d25783fa7490
Joni Laukkanen <jonilaukkanen1@gmail.com>
2021-09-24 22:45:56 +0300

    server.js

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
+require("dotenv").config();
+
 const express = require("express");
 const app = express();
 const jwt = require("jsonwebtoken");
+
+app.use(express.json());
 
 const events = [
   {
@@ -25,18 +29,31 @@ const events = [
   },
 ];
 
-app.get("/events", (req, res) => {
-  res.json(events);
+const users = [
+  {
+    username: "joni@j",
+    password: "$2a$10$vTpZSQQGAXaw9sohAzoEDu3HtYiHDxqcHzleAtT0gHAc5qNZYj9/y",
+    id: 1,
+  },
+  {
+    username: "joni@joni",
+    password: "$2a$10$lNS/XggRh4nJcFgXXnsnM.vdiT92Ftjnb.JM4Sp4hOc6yYC5/mPoC",
+    id: 2,
+  },
+];
+
+app.get("/users", (req, res) => {
+  res.json(users);
 });
 
-app.post("./login", (req, res) => {
+app.post("/login", (req, res) => {
   //auth
 
   const username = req.body.username;
   const user = { name: username };
 
   const accesToken = jwt.sign(user, process.env.ACCES_TOKEN_SECRET);
-  res.json({ accesToken, accesToken });
+  res.json({ accesToken: accesToken });
 });
 
 app.listen(3001);
